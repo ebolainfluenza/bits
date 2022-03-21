56330091ca2a085510d2af53962d05deb2d60c19
ebolainfluenza <6652415+ebolainfluenza@users.noreply.github.com>
2022-03-21 15:36:07 -0400

    simplify

--- mkrevmap2.c
+++ mkrevmap2.c
@@ -8,9 +8,9 @@ extern const unsigned int BitsInUint;
 int main(int argc, char **argv) {
     unsigned int i, x;
 
-    fprintf(stdout, "const unsigned int revmap2[] = {\n\t");
-    for (i = 0; i <= 0xff; i++) {
-        if (i && i % 16 == 0)    /* output 16 columns */
+    fprintf(stdout, "const unsigned int revmap2[] = {");
+    for (i = 0; i < 256; i++) {
+        if (i % 16 == 0)    /* output 16 columns */
             fprintf(stdout, "\n\t");
         x = reverseBits1(i);
         x = (x & 0xff << (BitsInUint - 8)) >> (BitsInUint - 8);
